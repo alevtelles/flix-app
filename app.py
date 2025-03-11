66217e861eb8e68ed00c0e96a0b258800ab4af1f
Valente Alexsander <alevtelles@gmail.com>
2025-03-11 10:32:54 -0300

    Iniciando projeto com stremalit

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+
+def main():
+    st.title('Flix App')
+
+    menu_option = st.sidebar.selectbox(
+        'Selecione uma opção',
+        ['Início', 'Gêneros', 'Atores/Atrizes', 'Filmes', 'Avaliações']
+    )
+
+    if menu_option == 'Início':
+        st.write('Bem vindo ao Flix App')
+    
+    if menu_option == 'Gêneros':
+        st.write('Lista de Gêneros')
+
+    if menu_option == 'Atores/Atrizes':
+        st.write('Lista de Atores/Atrizes')
+
+    if menu_option == 'Filmes':
+        st.write('Lista de Filmes')
+
+    if menu_option == 'Avaliações':
+        st.write('Lista de Avaliações')
+
+
+if __name__ == '__main__':
+    main()
