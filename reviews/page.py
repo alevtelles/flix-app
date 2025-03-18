51d8685d21d7db375ccc905d4cbb9a3f2f1b798d
Valente Alexsander <alevtelles@gmail.com>
2025-03-18 10:18:28 -0300

    criando service, repository e page de review

--- reviews/page.py
+++ reviews/page.py
@@ -1,22 +1,47 @@
 import pandas as pd
 import streamlit as st
 from st_aggrid import AgGrid
-
-reviews =[
-    {'id': 1, 'stars': 5}, 
-    {'id': 2, 'stars': 4}, 
-    {'id': 3, 'stars': 3}, 
-    {'id': 4, 'stars': 2}, 
-    {'id': 5, 'stars': 1}
-]
+from reviews.service import ReviewService
+from movies.service import MovieService
 
 
 def show_reviews():
-    st.write('Lista de Avaliações')
+    review_service = ReviewService()
+    reviews = review_service.get_reviews()
+    
 
-    AgGrid(
-        data=pd.DataFrame(reviews),
-        reload_data=True,
-        key='actors_grid',
-    )
+    if reviews: 
+        st.write('Lista de Avaliações')
+
+        reviews_df = pd.json_normalize(reviews)
+
+        AgGrid(
+            data=reviews_df,
+            reload_data=True,
+            key='actors_grid',
+        )
+    else:
+        st.warning('Nenhuma avaliação encontrada.')
+
+    st.title('Cadastrar nova avaliação')
+    
+    movie_service = MovieService()
+    movies = movie_service.get_movies()
+    movies_titles = {movie['title']: movie['id'] for movie in movies}
+    selected_movie_title = st.selectbox('Filme', list(movies_titles.keys()))
+
+    stars = st.number_input('Estrelas', min_value=1, max_value=5, step=1)
+    comment = st.text_area('Comentário')
+
+    if st.button('Cadastrar'):
+        new_review = review_service.create_review(
+            movie=movies_titles[selected_movie_title],
+            stars=stars,
+            comment=comment,
+        )
+        if new_review:
+            st.rerun()
+        else:
+            st.error('Erro ao cadastrar avaliação.')
+
 
